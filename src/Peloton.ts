00f89ca2c291087cc1320b9c5985df8176db7ecb
Matt Hackmann <matt.hackmann@gmail.com>
2023-04-17 21:48:42 -0700

    a bunch of session management

--- src/Peloton.ts
+++ src/Peloton.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-import { PelotonLoginResponse } from './types';
+import { PelotonLoginResponse, PelotonMeResponse } from './types';
 
 const BASE_URL = 'https://api.onepeloton.com';
 
@@ -12,11 +12,15 @@ enum HttpVerb {
 export class Peloton {
   private username: string;
   private password: string;
-  private session: string;
+  private sessionId: string;
   private userId: string;
 
-  constructor() {
+  constructor(sessionId?: string) {
+    this.sessionId = sessionId;
+  }
 
+  getSessionId() {
+    return this.sessionId;
   }
 
   async login(username: string, password: string) {
@@ -28,16 +32,42 @@ export class Peloton {
       password,
     });
 
-    this.session = response.session_id;
+    this.sessionId = response.session_id;
     this.userId = response.user_id;
   }
 
-  async workouts(count: number = 0, page: number = 0) {
-    if (!this.session) {
+  async getMe(): Promise<PelotonMeResponse> {
+    if (!this.sessionId) {
       throw new Error('Unable to fetch workouts before logging in');
     }
 
+    const response = await this._request<PelotonMeResponse>('api/me', HttpVerb.Get);
+
+    // stash this while we have it
+    this.userId = response.id;
+
+    return response;
+  }
+
+  async getWorkouts(count: number = 0, page: number = 0) {
+    if (!this.sessionId) {
+      throw new Error('Unable to fetch workouts before logging in');
+    }
+
+    // we need the user ID, so if we don't have it, get it
+    if (!this.userId) {
+      await this.getMe();
+    }
+
     return this._request(`api/user/${this.userId}/workouts?sort_by=-created&page=${page}&limit=${count}`, HttpVerb.Get);
+  }
+
+  async getWorkoutById(workoutId: string) {
+    if (!this.sessionId) {
+      throw new Error('Unable to fetch workouts before logging in');
+    }
+
+    return this._request(`api/workout/${workoutId}`, HttpVerb.Get);
   }
 
   private async _request<T>(path: string, method: HttpVerb = HttpVerb.Get, data?: Record<string, string>) {
@@ -47,7 +77,7 @@ export class Peloton {
       headers: {
         'Content-Type': 'application/json',
         'User-Agent': 'node-peloton',
-        'Cookie': this.session ? `peloton_session_id=${this.session}` : undefined,
+        'Cookie': this.sessionId ? `peloton_session_id=${this.sessionId}` : undefined,
       },
     });
 
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,26 @@
+export type PelotonWorkoutCount = {
+  name: string;
+  slug: string;
+  count: number;
+  icon_url: string;
+};
+
+export type PelotonMeResponse = {
+  id: string;
+  username: string;
+  location: string;
+  first_name: string;
+  middle_initial: string;
+  last_name: string;
+  name: string;
+  created_at: number;
+  total_workouts: number;
+  total_non_pedaling_metric_workouts: number;
+  workout_counts: PelotonWorkoutCount[];
+};
+
 export type PelotonLoginResponse = {
   session_id: string;
   user_id: string;
   pubsub_session: Record<string, unknown>;
-}
+};
